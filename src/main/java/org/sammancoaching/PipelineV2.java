841e383969ba496712367604d130b95afb0778ff
Bagelboi <donkeysoda22@gmail.com>
2026-03-11 16:55:43 -0300

    restruturar pipeline

---
+++ src/main/java/org/sammancoaching/PipelineV2.java
@@ -0,0 +1,82 @@
+package org.sammancoaching;
+
+import lombok.Getter;
+import org.sammancoaching.dependencies.Config;
+import org.sammancoaching.dependencies.Emailer;
+import org.sammancoaching.dependencies.Logger;
+import org.sammancoaching.dependencies.Project;
+
+public class PipelineV2 {
+    private final Config config;
+    private final Emailer emailer;
+    private final Logger log;
+
+    @Getter
+    private boolean testsPassed;
+    @Getter
+    private boolean deploySuccessful;
+    private Project project;
+
+    public PipelineV2(Config config, Emailer emailer, Logger log) {
+        this.config = config;
+        this.emailer = emailer;
+        this.log = log;
+    }
+
+    private void runTests() {
+        testsPassed = !project.hasTests() || project.runTests() == "success";
+
+        if (project.hasTests()) {
+            if (testsPassed) {
+                log.info("Tests passed");
+            } else {
+                log.error("Tests failed");
+            }
+        } else {
+            log.info("No tests");
+        }
+    }
+
+    private void deploy() {
+        deploySuccessful = testsPassed && project.deploy() == "success";
+
+        if (deploySuccessful) {
+            log.info("Deployment successful");
+        } else {
+            log.error("Deployment failed");
+        }
+
+    }
+
+    private void sendEmail() {
+        if (config.sendEmailSummary()) {
+            log.info("Sending email");
+
+            String email_content = "";
+
+            if (testsPassed) {
+                if (deploySuccessful) {
+                    email_content = "Deployment completed successfully";
+                } else {
+                    email_content = "Deployment failed";
+                }
+            } else {
+                email_content = "Tests failed";
+            }
+
+            emailer.send(email_content);
+        } else {
+            log.info("Email disabled");
+        }
+    }
+
+    public void run(Project _project) {
+        project = _project;
+        testsPassed = false;
+        deploySuccessful = false;
+
+        runTests();
+        deploy();
+        sendEmail();
+    }
+}
--- src/test/java/org/sammancoaching/PipelineTest.java
+++ src/test/java/org/sammancoaching/PipelineTest.java
@@ -2,19 +2,51 @@ package org.sammancoaching;
 
 import org.junit.jupiter.api.Test;
 import org.sammancoaching.dependencies.Project;
+import org.sammancoaching.dependencies.TestStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PipelineTest {
 
-    Pipeline pipeline = new Pipeline(new TestConfig(), new TestEmailer(), new CapturingLogger() );
+    PipelineV2 pipeline = new PipelineV2(new TestConfig(), new TestEmailer(), new CapturingLogger() );
 
     @Test
     void run() {
         pipeline.run(new Project.ProjectBuilder().build());
     }
 
+    @Test
+    void testsTest() {
+        Project project = Project.builder().setTestStatus(TestStatus.PASSING_TESTS).build();
+        pipeline.run(project);
+
+        assertTrue(pipeline.isTestsPassed());
+         project = Project.builder().setTestStatus(TestStatus.NO_TESTS).build();
+        pipeline.run(project);
+        assertTrue(pipeline.isTestsPassed());
+
+         project = Project.builder().setTestStatus(TestStatus.FAILING_TESTS).build();
+        pipeline.run(project);
+        assertFalse(pipeline.isTestsPassed());
+    }
+
+    @Test
+    void deployTest() {
+        //Deploy passa
+        Project project = Project.builder().setTestStatus(TestStatus.PASSING_TESTS).setDeploysSuccessfully(true).build();
+        pipeline.run(project);
+        assertTrue(pipeline.isDeploySuccessful());
+        //Deploy falha
+         project = Project.builder().setTestStatus(TestStatus.PASSING_TESTS).setDeploysSuccessfully(false).build();
+        pipeline.run(project);
+        assertFalse(pipeline.isDeploySuccessful());
+        //Testes falharam mas deploy é pra suceder
+        project = Project.builder().setTestStatus(TestStatus.FAILING_TESTS).setDeploysSuccessfully(true).build();
+        pipeline.run(project);
+        assertFalse(pipeline.isDeploySuccessful());
+    }
 }
