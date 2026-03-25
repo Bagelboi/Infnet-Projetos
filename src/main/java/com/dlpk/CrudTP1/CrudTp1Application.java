559778d9f401d786a26c1f5552961c453b94dbed
Bagelboi <donkeysoda22@gmail.com>
2026-03-25 00:06:33 -0300

    ugh

--- src/main/java/com/dlpk/CrudTP1/CrudTp1Application.java
+++ src/main/java/com/dlpk/CrudTP1/CrudTp1Application.java
@@ -1,6 +1,5 @@
 package com.dlpk.CrudTP1;
 
-import com.dlpk.CrudTP1.controller.BancoControllerCLI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
